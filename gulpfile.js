5919d9191c36d0c6495fe7be8def45cd96179aa1
var-bin <var-bin@users.noreply.github.com>
2018-04-13 20:06:37 +0300

    Added `autoprefixer` task

--- gulpfile.js
+++ gulpfile.js
@@ -2,30 +2,16 @@ const gulp = require("gulp");
 const postcss = require("gulp-postcss");
 const stylelint = require("stylelint");
 const gulpStylelint = require("gulp-stylelint");
+const autoprefixer = require("autoprefixer");
 
 const path = require("path");
 
 const APP_PATH = path.join(__dirname, "app");
 const STYLES_PATH = path.join(APP_PATH, "**/*");
+const DIST_PATH = path.join(APP_PATH, "dist");
 
 
 gulp.task("css:lint", () => {
-  function callback(file) {
-    const parsers = {
-      ".scss": require("postcss-scss"),
-      ".less": require("postcss-less")
-    };
-
-    return {
-      plugins: [
-        stylelint()
-      ],
-      options: {
-        parser: parsers[file.extname] || false
-      }
-    };
-  }
-
   return gulp.src(STYLES_PATH)
     .pipe(gulpStylelint({
       failAfterError: false,
@@ -43,4 +29,30 @@ gulp.task("css:lint-fix", () => {
       fix: true
     }))
     .pipe(gulp.dest(APP_PATH));
+});
+
+gulp.task("autoprefixer", () => {
+  function callback(file) {
+    const parsers = {
+      ".scss": require("postcss-scss"),
+      ".less": require("postcss-less")
+    };
+
+    return {
+      plugins: [
+        autoprefixer({
+          browsers: [
+            "last 5 versions"
+          ]
+        })
+      ],
+      options: {
+        parser: parsers[file.extname] || false
+      }
+    };
+  }
+
+  return gulp.src(STYLES_PATH, { base: "app"})
+    .pipe(postcss(callback))
+    .pipe(gulp.dest(DIST_PATH));
 });
